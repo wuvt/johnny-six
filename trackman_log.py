42c2c200be6486c4285e49ea8a506d4ba19fca3e
mutantmonkey <mutantmonkey@mutantmonkey.in>
2015-12-03 23:26:01 -0800

    move trackman URL/password config into config.liq

--- trackman_log.py
+++ trackman_log.py
@@ -3,18 +3,15 @@
 import os
 import requests
 
-submit_url = "http://localhost:9090/trackman/api/automation/log"
-submit_password = "hackme"
-
 data = {
-    'password': submit_password,
+    'password': os.environ['TRACKMAN_PASSWORD'],
     'album': os.environ.get('TRACK_ALBUM', ''),
     'artist': os.environ.get('TRACK_ARTIST', ''),
     'label': os.environ.get('TRACK_LABEL', ''),
     'title': os.environ.get('TRACK_TITLE', ''),
 }
 
-r = requests.post(submit_url, data=data)
+r = requests.post(os.environ['TRACKMAN_URL'], data=data)
 result = r.json()
 if 'success' in result:
     print("Track logged successfully")
