8817d9885e877432f550ed70ce8b6922acf47e0f
mutantmonkey <mutantmonkey@mutantmonkey.in>
2015-12-04 23:53:28 -0800

    update queue_underwriting.py to queue from dict

--- queue_underwriting.py
+++ queue_underwriting.py
@@ -1,31 +1,62 @@
 #!/usr/bin/python3
 
 import datetime
-import random
 import socket
 
-# TODO: keep a mapping of hours to sponsors and use that instead of picking
-# randomly
+socket_path = "/tmp/liquidsoap.sock"
+sponsors = {
+    # Monday
+    0: {
+    },
 
-choices = [
-    "/home/mutantmonkey/wuvt/automation/ua/mm.wav",
-    "/home/mutantmonkey/wuvt/automation/ua/UA - Greenhouse.mp3",
-    "/home/mutantmonkey/wuvt/automation/ua/UA - The Cellar.mp3",
-]
+    # Tuesday
+    1: {
+    },
+
+    # Wednesday
+    2: {
+    },
+
+    # Thursday
+    3: {
+    },
+
+    # Friday
+    4: {
+    },
+
+    # Saturday
+    5: {
+        0: "/home/mutantmonkey/wuvt/automation/ua/mm.wav",
+    },
+
+    # Sunday
+    6: {
+    },
+}
 
 now = datetime.datetime.now()
 
-sock = socket.socket(socket.AF_UNIX)
-sock.connect('/tmp/liquidsoap.sock')
-
-choice = random.choice(choices)
-cmd = "underwriting.push {}\n".format(choice).encode('utf-8')
-
-# queue a first one immediately
-sock.send(cmd)
-
 if now.minute > 30:
-    # we must be queuing for the next hour, so add another
+    # get underwriting for the next hour
+    now += datetime.timedelta(hours=1)
+
+if now.hour in sponsors[now.weekday()]:
+    sponsor = sponsors[now.weekday()][now.hour]
+    cmd = "underwriting.push {}\n".format(sponsor).encode('utf-8')
+
+    sock = socket.socket(socket.AF_UNIX)
+    sock.connect(socket_path)
+
+    # queue the first one immediately
     sock.send(cmd)
 
-sock.send(b'exit\n')
+    if now.minute > 30:
+        # we must be queuing for the next hour, so add the second
+        sock.send(cmd)
+
+    sock.send(b'exit\n')
+
+    print("Underwriting queued")
+else:
+    print("No underwriting to queue for {}".format(now))
