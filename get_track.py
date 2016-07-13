c1f6a8ff317de60849a06e68b006265ab73b2d4d
mutantmonkey <mutantmonkey@mutantmonkey.in>
2016-07-12 20:49:53 -0700

    also check if now.hour == 11 on Saturday

--- get_track.py
+++ get_track.py
@@ -19,7 +19,7 @@ elif now.hour < 9:
     now = now.replace(hour=6, minute=0)
 elif now.hour < 14:
     # special case for Saturday: only play children's music until 11
-    if now.isoweekday() == 7 and now.hour > 11:
+    if now.isoweekday() == 7 and now.hour >= 11:
         now = now.replace(hour=11, minute=0)
     else:
         now = now.replace(hour=9, minute=0)
