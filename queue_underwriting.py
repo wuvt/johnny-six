975d3bb644ee9b33f277f2f244a22edbefb217e8
echarlie <echarlie@vtluug.org>
2017-11-10 18:30:58 -0500

    Make flake8 happy, better logging, more idiomatic python
    I used random.choice in some places, and .format to add the current
    date. I did some testing to make sure it all works.
    
    \begin{rant}
    Flake8's guidelines are utterly inane imo; the code is now /less/
    readable (matching parens w/out whitespace is hard, and lines under 79
    chars are dumb in an indentation-based language.)
    \end{rant}
    
    	ECL

--- queue_underwriting.py
+++ queue_underwriting.py
@@ -5,12 +5,14 @@ import datetime
 import calendar
 import socket
 import random
+import sys
 
 underwritingSchedule = '/tmp/sample.yml'
 socketPath = "/tmp/liquidsoap.sock"
 
 underwriters = yaml.safe_load(open(underwritingSchedule))
 thisHour = []
+
 
 def push_command(url):
     cmd = "underwriting.push {}\n".format(url).encode('utf-8')
@@ -28,44 +30,51 @@ def push_command(url):
     sock.send(b'exit\n')
     return True
 
+
 # check which hour we're queuing for and return datetime object
 def what_hour():
     queue_hour = datetime.datetime.now()
     if queue_hour.minute > 30:
-        #queuing for next hour
+        # queuing for next hour
         queue_hour = queue_hour + datetime.timedelta(hours=1)
     return queue_hour
+
 
 queue_hour = what_hour()
 
 for underwriting in underwriters:
-    today=queue_hour.date()
-    weekday=calendar.day_name[queue_hour.weekday()].lower()
+    today = queue_hour.date()
+    weekday = calendar.day_name[queue_hour.weekday()].lower()
     # we want to sanely handle do-not-kill situations
-    if ( ( not underwriting['startdate'] or underwriting['startdate'] <= today ) and\
-            ( not underwriting['enddate'] or underwriting['enddate'] >= today ) ) and\
-            weekday in underwriting['schedule'] and queue_hour.hour in underwriting['schedule'][weekday]:
+    if ((not underwriting['startdate'] or
+        underwriting['startdate'] <= today) and
+            (not underwriting['enddate'] or
+                underwriting['enddate'] >= today))and\
+            weekday in underwriting['schedule'] and\
+            queue_hour.hour in underwriting['schedule'][weekday]:
         if len(underwriting['url']) > 1:
             # Then we probably have multiple recordings of the grant statement
-            playFile = underwriting['url'][random.randint(0,len(underwriting['url'])-1)]
+            playFile = random.choice(underwriting['url'])
         else:
-            # we probably don't need this, since a random int between 0 and 0 is 0
+            # we probably don't need this, since a random int between 0 and 0
+            # is 0
             playFile = underwriting['url'][0]
-        # probably the safer way to handle things in the future, but would require more parsing
-        #thisHour[underwriting['name']] = playFile
+        # probably the safer way to handle things in the future, but would
+        # require more parsing
+        # thisHour[underwriting['name']] = playFile
         thisHour.append(playFile)
 
 # because automation currently only plays one piece of underwriting at a time,
 # we need arbitrate which piece we push. We'll pick a random one!
 if len(thisHour) is not 0:
-    if push_command(thisHour[random.randint(0,len(thisHour)-1)]):
+    if push_command(random.choice(thisHour)):
         # ideally, we would list what underwriting was queued, but we're being
         # kind of sloppy with cases of overlapping underwriting
         if len(thisHour) > 1:
-            print("There are {} underwriters this hour!".format(len(thisHour) ) )
-        print("Underwriting for the hour queued")
+            print("There are {} underwriters this hour!".format(len(thisHour)))
+        print("Underwriting queued for {}".format(queue_hour))
     else:
-        print("Failed to queue underwriting")
-        exit(1)
+        print("Failed to queue underwriting for {}".format(queue_hour))
+        sys.exit(1)
 else:
-    print("No underwriting for the hour")
+    print("No underwriting to queue for {}".format(queue_hour))
